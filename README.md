9b484beb68ebbff17fad86d7d546d684e58eb2a5
Xtreme Cookiez <10819480+XtremeCookiez@users.noreply.github.com>
2024-01-20 20:51:06 -0600

    trying some work?

--- README.md
+++ README.md
@@ -7,6 +7,9 @@ couple things. One, you could have a dynamic list of authorized users on a share
 the users via pull requests. Two, for personal use this allows very quick and easy authorization to
 a machine. And if you need to later add another key, you can do so very easily.
 
-## Requirements
+## Repository Structure
 
-- jq
+The repositories that are used to hold public keys are expected in one of 2 formats. Either it
+contains a `public_keys` folder which holds a series of public key files in the format
+`<file-name>.pub`. Or it contains an `authorized_keys` file which holds a public key on each line of
+the file.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
+use config::Config;
+use std::{collections::HashSet, env::temp_dir, hash::Hash};
+use git2::Repository;
+use tempdir::TempDir;
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hasher};
+use std::path::Path;
+
+fn debug_print(debug: bool, msg: String) {
+    if debug {
+        println!("{}", msg);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let debug: bool = true;
+
+    let config: Config = Config::builder()
+        .add_source(config::File::with_name("/etc/git-public-keys/config").required(false))
+        .add_source(config::File::with_name("config").required(false))
+        .build()
+        .unwrap();
+
+    let urls = config.get::<Vec<String>>("repos.urls").unwrap();
+    debug_print(debug, format!("{:?}", urls));
+
+    for url in urls {
+        let mut s = DefaultHasher::new();
+        url.hash(&mut s);
+        let hash = format!("{:x}", s.finish());
+        let tmp_dir = TempDir::new(&hash).unwrap();
+        debug_print(debug, format!("{:?}", tmp_dir.path()));
+
+        let repo = git2::Repository::clone(&url, Path::new("tmp/pubkeys")).unwrap();
+        let tmp_dir_path = tmp_dir.path();
+
+        let auth_keys = tmp_dir_path.join("authorized_keys");
+        for entry in tmp_dir_path.read_dir().expect("read_dir call failed") {
+            if let Ok(entry) = entry {
+                println!("LS: {:?}", entry.path());
+            }
+        }
+        println!("{:?}", auth_keys);
+        if auth_keys.exists() {
+            println!("neat");
+        }
+        // if 
+    }
 }
